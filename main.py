4ee977035dfec6d38e71649002f0dff7a9ddcfe5
mefolder <40847223+mefolder@users.noreply.github.com>
2018-07-08 12:17:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,10 @@ def presskeys(memkey): # list
         else:
             with open('/dev/hidg0','rb+') as hidg:
                 hidg.write(i.encode())
-def readconfig(config): # see example.conf
-    file=[]
+def presskeysfromconfig(config): # see example.conf
     with open(config) as f:
         for line in f:
-            file.append(line)
-    return file
+            presskeys(getvalueofstr(i).encode())
 repeatnextcommand=1
 def getvaluesofstr(string):
     keys={'a': 4, 'b': 5, 'c': 6, 'd': 7, 'e': 8, 'f': 9, 'g': 10, 'h': 11, 'i': 12, 'j': 13, 'k': 14, 'l': 15, 'm': 16, 'n': 17, 'o': 18, 'p': 19, 'q': 20, 'r': 21, 's': 22, 't': 23, 'u': 24, 'v': 25,'w':26,'x':27,'y':28,'z':29,'1':30,'2':31,'3':32,'4':33,'5':34,'6':35,'7':36,'8':37,'9':38,'0':39, ' ':44,'-':45,'=':46,'[':47,']':48,'\':49,";":51,''':52,',':54,'.':55}
@@ -63,5 +61,4 @@ def getvaluesofstr(string):
         repeatnextcommand=int(string[0])
     return output
 if __name__ == '__main__':
-    for i in readconfig(sys.argv[-1]):
-        presskeys(getvaluesofstr(i))
+    presskeysfromconfig(sys.args[-1])
